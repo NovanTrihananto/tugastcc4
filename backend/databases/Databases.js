545a316116d607b8ecc8c376214b50d9d994a61a
NovanTrihananto <152064002+NovanTrihananto@users.noreply.github.com>
2025-03-17 00:47:22 +0700

    Update Databases.js

--- backend/databases/Databases.js
+++ backend/databases/Databases.js
@@ -1,27 +1,7 @@
 import { Sequelize } from "sequelize";
-
-// Koneksi ke database MySQL di GCP
+// Nyambungin db ke BE
 const db = new Sequelize("notes", "root", "", {
-  host: "34.60.102.191",
-  dialect: "mysql",
-  port: 3306,
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
+ host: "localhost",
+ dialect: "mysql",
 });
-
-// Tes koneksi
-(async () => {
-  try {
-    await db.authenticate();
-    console.log("✅ Database Connected to GCP!");
-  } catch (error) {
-    console.error("❌ Database connection failed:", error);
-  }
-})();
-
 export default db;
